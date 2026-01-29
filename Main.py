c306df70718947f7e9dd1cada57e56cf7f1861c7
Mayank malviya <mayankmalviya440@gmail.com>
2026-01-29 10:06:18 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
+import random
+import string
 
+print("🔐 Welcome to Password Generator developed be Monty")
+
+length = int(input("Enter the length of the password: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Your generated password is:", password)
